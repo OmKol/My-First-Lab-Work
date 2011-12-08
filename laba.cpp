337018d1d5e7146083d6dac93eb73b31f4302fc4
OmKol <omkol@omkol-1215N.(none)>
2011-12-08 12:11:18 +0400

    Добавлена структура и добавление элементов

--- laba.cpp
+++ laba.cpp
@@ -5,6 +5,52 @@
  *      Author: omkol
  */
 
-int main() {
+#include <stdio.h>
 
+struct Node
+{
+	int d;
+	Node *next;
+	Node *prev;
+};
+
+Node *add(Node *e, int val)
+{
+	Node *n = new Node();
+
+	n->prev = e;
+	n->next = e->next;
+	if(e->next)
+		e->next->prev = n;
+	e->next = n;
+	n->d = val;
+	return n;
+}
+
+int main() {
+	Node first, *last;
+	last = &first;
+	first.next = 0;
+	first.prev = 0;
+
+	last = add(last, 0);
+	last = add(last, 1);
+	last = add(last, 2);
+
+	Node *cur = first.next;
+	while(cur)
+	{
+		printf("%d\n", cur->d);
+		cur = cur->next;
+	}
+
+	cur = first.next;
+	while(cur)
+	{
+		Node *next = cur->next;
+		delete cur;
+		cur = next;
+	}
+
+	return 0;
 }
