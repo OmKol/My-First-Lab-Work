ee3374700e7756d237814e672519d9dcb7f571dd
OmKol <omkol@omkol-1215N.(none)>
2011-12-09 12:17:45 +0400

    Сортировка вставками

--- laba.cpp
+++ laba.cpp
@@ -27,17 +27,53 @@ Node *add(Node *e, int val)
 	return n;
 }
 
+Node *insert(Node *pr, Node *ne, int val)
+{
+	Node *n = new Node();
+
+	n->prev = pr;
+	n->next = ne;
+	pr->next = n;
+	ne->prev = n;
+	n->d = val;
+	return n;
+}
+
 int main() {
 	Node first, *last;
 	last = &first;
 	first.next = 0;
 	first.prev = 0;
 
-	last = add(last, 0);
-	last = add(last, 1);
 	last = add(last, 2);
+	last = add(last, 5);
+	last = add(last, 8);
 
 	Node *cur = first.next;
+	while(cur)
+	{
+		printf("%d\n", cur->d);
+		cur = cur->next;
+	}
+
+	int temp = 0;
+	scanf("%d", &temp);
+
+	bool check = true;
+	cur = first.next;
+	while(cur)
+	{
+		if (temp < cur->d)
+		{
+			insert(cur->prev, cur, temp);
+			check = false;
+			break;
+		}
+		cur = cur->next;
+	}
+	if (check) last = add(last, temp);
+
+	cur = first.next;
 	while(cur)
 	{
 		printf("%d\n", cur->d);
